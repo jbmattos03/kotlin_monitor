75ea546891df466f2d3d8d918333fa49ab7f03f6
jbmattos03 <jbmattos2003@gmail.com>
2025-05-09 14:49:37 -0300

    fix: :fire: Remove useless BENCHMARK_MODE variable from env.sh

--- env.sh
+++ env.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 IP_ADDRESS=$(ip route get 8.8.8.8 | grep -oP 'src \K[^ ]+')
 HOST=$(hostnamectl | grep -oP "Static hostname\s*: \K.+")
-BENCHMARK=false
-
-while getopts "bh" opt; do
-  case $opt in
-    b)
-        BENCHMARK=true
-        ;;
-    h)
-        echo "Usage: $0 [-b] [-h]"
-        echo "  -b  Enable benchmark mode"
-        echo "  -h  Show this help message"
-        exit 0
-        ;;
-    :)
-        echo "Invalid argument" >&2
-        exit 1
-        ;;
-  esac
-done
 
 # Check if the .env file exists; if not, create it
 if [ ! -e ".env" ]; then
@@ -42,13 +23,4 @@ if grep -q "^HOST=" ./.env; then
     sed -i "s/^HOST=.*/HOST=$HOST/" ./.env
 else
     echo "HOST=$HOST" >> ./.env
-fi
-
-# Check if the BENCHMARK_MODE variable exists in the .env file
-# If it does, update it
-# If not, add it
-if grep -q "^BENCHMARK_MODE=" ./.env; then
-    sed -i "s/^BENCHMARK_MODE=.*/BENCHMARK_MODE=$BENCHMARK/" ./.env
-else
-    echo "BENCHMARK_MODE=$BENCHMARK" >> ./.env
 fi
