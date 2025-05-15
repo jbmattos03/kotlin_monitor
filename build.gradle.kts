c5385849f014a92de93beb8705156f9f91028088
jbmattos03 <jbmattos2003@gmail.com>
2025-05-15 11:57:38 -0300

    build(Multiplatform): :construction_worker: Update com.android.application to v8.10.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("multiplatform") version "2.1.20"
-    id("com.android.application") version "8.4.0"
+    id("com.android.application") version "8.10.0"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.1.20"
 }
 
