9e56a9e2af5d8eb992f38279c8c34529e9120ded
jbmattos03 <jbmattos2003@gmail.com>
2025-07-29 15:48:32 -0300

    build(jvm): Add slf4j for structured logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,9 @@ kotlin {
                 implementation("io.opentelemetry:opentelemetry-exporter-otlp:1.49.0")
                 implementation("com.github.oshi:oshi-core:6.8.1")
                 implementation("net.java.dev.jna:jna-platform:5.17.0")
-                implementation("org.slf4j:slf4j-simple:2.0.17")
+                implementation("ch.qos.logback:logback-classic:1.4.14")
+                implementation("org.slf4j:slf4j-api:2.0.13")
+                //implementation("org.slf4j:slf4j-simple:2.0.17")
                 implementation("io.github.cdimascio:dotenv-kotlin:6.5.1")
             }
         }
